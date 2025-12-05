7b91bed005a8bdb2415dccf08f32d5b341a28ee9
Africa Galindo <aesplea0302@g.educaand.es>
2025-12-05 19:59:27 +0100

    BatallaNaval Estudiar para examen

--- BatallaNaval.py
+++ BatallaNaval.py
@@ -1,3 +1,17 @@
-def class PlataformaNaval:
-    def __init__(self, nombre: str, pais: str, eslora: double):
+class PlataformaNaval:
+    def __init__(self, nombre: str, pais: str, eslora: float, desplazamiento: float, VelocidadMaxima: float):
+        self.nombre = nombre
+        self.pais = pais
+        self.eslora = float(eslora)
+        self.desplazamiento = float(desplazamiento)
+        self.VelocidadMaxima = float(VelocidadMaxima)
     
+    def __str__(self):
+        return (f"Nombre: {self.nombre}\n"
+                f"Pais: {self.pais}\n"
+                f"Eslora: {self.eslora}\n"
+                f"Desplazamiento: {self.desplazamiento}\n"
+                f"Velocidad  Máxima: {self.VelocidadMaxima}\n")
+
+    def navegar(self,rumbo: float, velocidad: float):
+        pass
